c6006537a02a14cdcdfce082e5f98ceb497785c0
itc- s23025 <s23025@std.it-college.ac.jp>
2023-05-24 06:41:32 +0000

    add head

---
+++ chap3/p39.py
@@ -0,0 +1,5 @@
+
+home = "america"
+
+result = "t1" if home == "Japan" else "f1"
+print(result)
---
+++ chap3/thhvy6g3.py
@@ -0,0 +1,5 @@
+
+if home == "Japan":
+    print("Hello, America")
+else:
+    print("Hello, World!")
